d4c9c02a0b5aeff59bc9f5fce9ba69084cce0246
pejovskis <pejovskis2@gmail.com>
2023-07-24 14:23:03 +0200

    Add Expense Update

--- app/src/main/java/com/example/myapp10/MainActivity.java
+++ app/src/main/java/com/example/myapp10/MainActivity.java
@@ -7,13 +7,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -25,14 +32,76 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
     private boolean showOneTapUI = true;
+    private BeginSignInRequest signInRequest;
     private SignInClient oneTapClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(this);
+    public void onCreate(@Nullable Bundle savedInstanceState,
+                         @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
         oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+        // ...
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if the user is already signed in before displaying the One Tap UI.
+        // If the user is signed in, you might want to redirect them to the main activity.
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            // User is already signed in, handle it accordingly.
+            // For example, start the main activity.
+        } else {
+            // User is not signed in, show the One Tap UI.
+            showOneTapUI = true;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // If showOneTapUI is true, it means the user has not declined to use One Tap sign-in.
+        // So, show the One Tap sign-in UI in onResume to prevent it from showing multiple times.
+        if (showOneTapUI) {
+            showOneTapUI = false; // Set to false to prevent repeated prompts.
+            oneTapClient.beginSignIn(signInRequest)
+                    .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
+                        @Override
+                        public void onSuccess(BeginSignInResult result) {
+                            try {
+                                startIntentSenderForResult(
+                                        result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+                                        null, 0, 0, 0);
+                            } catch (IntentSender.SendIntentException e) {
+                                Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                            }
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // No saved credentials found. Continue presenting the signed-out UI.
+                            Log.d(TAG, e.getLocalizedMessage());
+                        }
+                    });
+        }
     }
 
     @Override
@@ -44,26 +113,22 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
                     String idToken = credential.getGoogleIdToken();
+                    String username = credential.getId();
+                    String password = credential.getPassword();
+
                     if (idToken != null) {
-                        // Got an ID token from Google. Use it to authenticate with Firebase.
-                        FirebaseAuth.getInstance().signInWithCredential(GoogleAuthProvider.getCredential(idToken, null))
-                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if (task.isSuccessful()) {
-                                            // Sign-in success, update UI with the signed-in user's information
-                                            Log.d(TAG, "signInWithCredential:success");
-                                            FirebaseUser user = task.getResult().getUser();
-                                            // Proceed with your app logic after successful sign-in
-                                        } else {
-                                            // If sign-in fails, display a message to the user.
-                                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                                        }
-                                    }
-                                });
+                        // Got an ID token from Google. Use it to authenticate with your backend.
+                        Log.d(TAG, "Got ID token: " + idToken);
+                        // You can send this ID token to your server and verify it there.
+                        // After verifying, you can sign in the user on your server.
+                    } else if (password != null) {
+                        // Got a saved username and password. Use them to authenticate with your backend.
+                        Log.d(TAG, "Got password: " + password);
+                        // You can use these credentials to sign in the user directly on your server.
                     }
                 } catch (ApiException e) {
-                    Log.d("Error", "Error checking if it's you");
+                    // Handle API exceptions if any.
+                    Log.e(TAG, "Error getting credential from result: " + e.getLocalizedMessage());
                 }
                 break;
         }
